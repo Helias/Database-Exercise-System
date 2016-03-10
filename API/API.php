d9cd12aeedbb6f02a928c1a43c47e2aa2074b4d4
Helias <stefanoborzi32@gmail.com>
2016-03-10 22:27:45 +0100

    Implemented first API to get table content

--- API/API.php
+++ API/API.php
@@ -3,9 +3,10 @@ include "config.php";
 
 $db = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
 
-foreach($db->query('SELECT * FROM argomenti') as $row)
-{
-    print_r($row);
-}
+// table
+$stmt = $db->query('SELECT * FROM '.$_GET['table']);
+$json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 
+header('Content-Type: application/json');
+echo $json;
 ?>
