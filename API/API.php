d30fa9df3506a57fbf21e8398aadf14a8555cd9b
Tkd-Alex <alex.tkd.alex@gmail.com>
2016-03-14 22:35:55 +0100

    Try to write the api for admin panel

--- API/API.php
+++ API/API.php
@@ -9,7 +9,7 @@ if (isset($_GET['arguments']))
     $json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 }
 else if (isset($_GET['exerciseSQL']) && $_GET['exerciseSQL'] != "")
-{
+{	
     // select all SQL exercises for argument
     $stmt = $db->query('SELECT * FROM domandeSQL WHERE argomento = ' . $_GET['exerciseSQL']);
     $json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
@@ -20,6 +20,13 @@ else if (isset($_GET['exerciseALG']))
     $stmt = $db->query('SELECT * FROM domandeALG WHERE argomento = ' . $_GET['exerciseALG']);
     $json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 }
+/*else if (isset($_GET['argument'])) && (isset($_GET['textQ'])) && (isset($_GET['resultQuery']))
+{
+   
+    $stmt = $db->query('INSERT INTO des.soluzioni(soluzione) VALUES ('.$_GET['resultQuery'].');');
+    $stmt = $db->query('INSERT INTO des.domandeALG(testo,soluzione,argomento) VALUES ('.$_GET['argument'].','.$_GET['textQ'].','.$_GET['resultQuery'].');');
+    //$json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+}*/
 
 header('Content-Type: application/json');
 echo $json;
