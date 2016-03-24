94f9b04488b0ce1faf0b4632cedfe5c41d91cb9c
Tkd-Alex <alex.tkd.alex@gmail.com>
2016-03-24 20:48:53 +0100

    Clear the api.

--- API/API.php
+++ API/API.php
@@ -20,13 +20,6 @@ else if (isset($_GET['exerciseALG']))
     $stmt = $db->query('SELECT * FROM domandeALG WHERE argomento = ' . $_GET['exerciseALG']);
     $json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 }
-/*else if (isset($_GET['argument'])) && (isset($_GET['textQ'])) && (isset($_GET['resultQuery']))
-{
-   
-    $stmt = $db->query('INSERT INTO des.soluzioni(soluzione) VALUES ('.$_GET['resultQuery'].');');
-    $stmt = $db->query('INSERT INTO des.domandeALG(testo,soluzione,argomento) VALUES ('.$_GET['argument'].','.$_GET['textQ'].','.$_GET['resultQuery'].');');
-    //$json = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-}*/
 
 header('Content-Type: application/json');
 echo $json;
